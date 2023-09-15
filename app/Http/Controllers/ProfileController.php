135817b562a0a4e726b6e9bb58263f2faa485769
Nguemoue <lucchuala@kamix.io>
2023-09-15 19:58:00 +0100

    reset

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
 
 class ProfileController extends Controller
 {
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+    public function edit(Request $request):View
     {
         return view('profile.edit', [
             'user' => $request->user(),
